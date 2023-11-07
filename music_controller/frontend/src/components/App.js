4c9fe73e7b4dad3f33bd03550f66b99b06d246d7
dominiquealvarez8888@gmail.com <dominiquealvarez8888@gmail.com>
2023-11-08 00:24:20 +0800

    dev fix

---
+++ music_controller/frontend/src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+export const App = () => {
+  return (
+    <div><h1>Test, Please work</h1></div>
+  )
+}
+
+export default App();
+
+const appDiv = document.getElementById("app");
+render(<App />, appDiv);
