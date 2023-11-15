e54d2dd29ad589d1c08f56760fbb98f70e755e1b
dominiquealvarez8888@gmail.com <dominiquealvarez8888@gmail.com>
2023-11-15 12:58:37 +0800

    reverted

---
+++ music_controller/frontend/src/components/HomePage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { RoomJoinPage } from "./RoomJoinPage";
+import { CreateRoomPage } from "./CreateRoomPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+
+export const HomePage = (props) => {
+  return  (
+      <Router>
+        <Routes>
+          <Route path="/" element={<p>This is the home page</p>} />
+          <Route path="/join" element={<RoomJoinPage />} />
+          <Route path="/create" element={<CreateRoomPage />} />
+        </Routes>
+      </Router>
+    );
+}
